6e644014139285560e6d1a306504d77d395967a1
aghosh0605 <aghosh0605@gmail.com>
2021-06-10 16:40:05 +0530

    🔥 Some unnecessary removes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Polyline,
+} from "@react-google-maps/api";
 
-function App() {
+const MapContainer = () => {
+  const mapContainerStyle = {
+    height: "400px",
+    width: "800px",
+  };
+
+  const center = {
+    lat: 0,
+    lng: -180,
+  };
+
+  const onLoad = (polyline) => {
+    console.log("polyline: ", polyline);
+  };
+
+  const path = [
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+  ];
+
+  const options = {
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    zIndex: 1,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyC6DN2P1h7GqFozWGvMhVc3RR-cOLUBDnM">
+      <GoogleMap
+        id="marker-example"
+        mapContainerStyle={mapContainerStyle}
+        zoom={2}
+        center={center}
+      >
+        <Polyline onLoad={onLoad} path={path} options={options} />
+        {path.map((item) => {
+          return <Marker position={item} draggable={false} />;
+        })}
+      </GoogleMap>
+    </LoadScript>
   );
-}
-
-export default App;
+};
+export default MapContainer;
