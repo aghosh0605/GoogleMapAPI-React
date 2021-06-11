5b64e519b82b7dced631d5c4b30af3def6416935
aghosh0605 <aghosh0605@gmail.com>
2021-06-11 18:07:56 +0530

    ⚡️ Improved Working Spaces

--- src/App.js
+++ src/App.js
@@ -1,61 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GoogleMap,
   LoadScript,
   Marker,
   Polyline,
 } from "@react-google-maps/api";
+import Card from "./Card";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import options from "./Constrains";
 
+let path = [];
 const MapContainer = () => {
-  const mapContainerStyle = {
-    height: "400px",
-    width: "800px",
-  };
+  const [content, setContent] = useState(
+    "Click on Log Button to see the co-ordinates you clicked! Click Clear Button to clear the co-ordinates"
+  );
+  const [marker, setMarker] = useState([]);
+  const [polyline, setPolyline] = useState([]);
 
   const center = {
     lat: 0,
     lng: -180,
   };
 
-  const onLoad = (polyline) => {
-    console.log("polyline: ", polyline);
-  };
+  function _onClick(obj) {
+    let location = { lat: obj.latLng.lat(), lng: obj.latLng.lng() };
+    path.push(location);
+  }
 
-  const path = [
-    { lat: 37.772, lng: -122.214 },
-    { lat: 21.291, lng: -157.821 },
-    { lat: -18.142, lng: 178.431 },
-    { lat: -27.467, lng: 153.027 },
-  ];
+  let text;
+  function logfile() {
+    path.map((data) => {
+      text = text + JSON.stringify(data);
+      return text;
+    });
+    setContent(
+      "Click on Log Button to see the co-ordinates you clicked! Click Clear Button to clear the co-ordinates" +
+        text
+    );
+    setPolyline(path);
+    setMarker(path);
+  }
 
-  const options = {
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    radius: 30000,
-    zIndex: 1,
-  };
+  function clearfile() {
+    path = [];
+    setContent(
+      "Click on Log Button to see the co-ordinates you clicked! Click Clear Button to clear the co-ordinates"
+    );
+    setPolyline([]);
+    setMarker([]);
+  }
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyC6DN2P1h7GqFozWGvMhVc3RR-cOLUBDnM">
-      <GoogleMap
-        id="marker-example"
-        mapContainerStyle={mapContainerStyle}
-        zoom={2}
-        center={center}
-      >
-        <Polyline onLoad={onLoad} path={path} options={options} />
-        {path.map((item) => {
-          return <Marker position={item} draggable={false} />;
-        })}
-      </GoogleMap>
-    </LoadScript>
+    <div>
+      <div className="left_map">
+        <LoadScript googleMapsApiKey="AIzaSyC6DN2P1h7GqFozWGvMhVc3RR-cOLUBDnM">
+          <GoogleMap
+            onClick={_onClick}
+            id="marker-example"
+            mapContainerStyle={{ height: "100%" }}
+            zoom={2}
+            center={center}
+          >
+            <Polyline path={polyline} options={options} />
+            {marker.map((item) => {
+              return <Marker position={item} draggable={false} />;
+            })}
+          </GoogleMap>
+        </LoadScript>
+      </div>
+      <div className="taskbar">
+        <Button className="success" onClick={logfile}>
+          Log
+        </Button>
+        <Button className="clear" onClick={clearfile}>
+          clear
+        </Button>
+        <Card text={content} />
+      </div>
+    </div>
   );
 };
 export default MapContainer;
